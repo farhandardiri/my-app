417aa6a90e8ff50a3471d596d8cdfba2688879d7
farhandardiri <faran.f4124n@gmail.com>
2021-09-18 19:07:48 +0700

    add login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Coba from './pages/Coba';
+import Login from './components/Login';
 
 
 function App() {
  
   return (
     <Router>
-    <div className="App">
-      <Navbar/>
+    <div className="App bg-gray-100">
+      {/* <Navbar/> */}
       {/* Content */}
-      <div className="content">
-        <div className="max-w-md mx-auto rounded-xl lg:mt-2 mt-24  md:max-w-full lg:max-w-6xl">
+      {/* <div className="content">
+        <div className="max-w-md mx-auto rounded-xl lg:mt-2 mt-24  md:max-w-full lg:max-w-6xl"> */}
           <Switch>
-            <Route exact path="/">
-             <Dashboard/>
-            </Route>
-            <Route path="/coba">
-              <Coba/>
+            <Route path="/" component={Login}>
+             <Login/>
             </Route>
           </Switch>
-        </div>
-        </div>
+        {/* </div>
+        </div> */}
       {/* akhir Content */}
     </div>
     </Router>
